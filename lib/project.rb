49681b92f7105c81284b67e74a3aba57420a26a3
Sav Younts <savyounts@gmail.com>
2018-08-10 19:02:15 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,12 +7,11 @@ class Project
     @title = title
   end
   
-  def create_project
+ 
     
   
   def add_backer(backer)
-    new_backer = Backer.new(backer)
-    self.backers << new_backer.name 
-    new_backer.backed_projects << self.title
+    self.backers << backer.name 
+    backer.backed_projects << self.title
   end
 end
