8465a64cc8cff5d0db117472531caaf69e184b60
Sav Younts <savyounts@gmail.com>
2018-08-10 19:04:40 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Backer
+  attr_accessor :backed_projects, :name
+  
+  def initialize(name)
+    @backed_projects = []
+    @name = name
+  end
+  
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self 
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Project
+  attr_accessor :backers, :title
+  def initialize(title)
+    @backers = []
+    @title = title
+  end
+  
+ 
+    
+  
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self
+  end
+end
