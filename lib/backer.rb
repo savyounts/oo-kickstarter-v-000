851e9504defa8eb26acfeb65005f6698981980b6
Sav Younts <savyounts@gmail.com>
2018-08-10 18:33:57 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,6 @@ class Backer
   
   def back_project(project)
     project = Project.new(project)
-    project.title = project 
     self.backed_projects << project.title unless self.backed_projects.include?(project.title)
     project.backers << self.name 
    binding.pry
